8cdf1add2bd05f97a0428139e644426c2579f701
Jan Nowaczek <jasiek19_85@o2.pl>
2019-04-07 13:56:36 +0200

    Prop in Square component added

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 
 class Square extends React.Component {
     render() {
         return (
             <button className="square">
-                {/* TODO */}
+                {this.props.value}
             </button>
         );
     }
@@ -12,11 +12,12 @@ class Square extends React.Component {
 
 class Board extends React.Component {
     renderSquare(i) {
-        return <Square />;
+        return <Square />
+        return <Square value={i} />
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: X'
 
         return (
             <div>
